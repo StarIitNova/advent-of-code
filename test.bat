0f2e58fb78ad81a9e99732fa40a4a4931b02d9bf
Fynotix <dollorrblx@gmail.com>
2023-12-10 12:48:41 -0700

    removed outdated flags

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set OUTPUT=bin/aoc
-set CFLAGS=-Isource/ -std=c++17 -D_AOC_USE_OSSL
+set CFLAGS=-Isource/ -std=c++17 -g -Og
 set LFLAGS=
 
 c++ %CFLAGS% -o %OUTPUT% source/%1.cpp source/tools/md5.cpp %LFLAGS% && bin\aoc.exe
