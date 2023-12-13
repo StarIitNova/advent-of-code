3f6b5529c40734a742d8fa53227466c9e1fac394
Dollor-Lua <dollorrblx@gmail.com>
2023-12-13 16:40:27 -0700

    core.h update to sync day 13

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-g++ -Isource/ -O2 -std=c++17 -o bin/aoc source/$1.cpp source/tools/md5.cpp && time ./bin/aoc
+g++ -Isource/ -Og -g -std=c++17 -o bin/aoc source/$1.cpp source/tools/md5.cpp && ./bin/aoc
