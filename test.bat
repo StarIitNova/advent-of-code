32baee1ee4d0ca3116a9c093c3424b3ee2861f71
Fynotix <dollorrblx@gmail.com>
2023-12-12 19:19:25 -0700

    increased optimization

--- test.bat
+++ test.bat
@@ -1,7 +1,15 @@
 @echo off
 
 set OUTPUT=bin/aoc
-set CFLAGS=-Isource/ -std=c++17 -g -Og
+set CFLAGS=-Isource/ -std=c++17 -O2
 set LFLAGS=
 
-c++ %CFLAGS% -o %OUTPUT% source/%1.cpp source/tools/md5.cpp %LFLAGS% && bin\aoc.exe
+echo -- Compiling AoC Script --
+c++ %CFLAGS% -o %OUTPUT% source/%1.cpp source/tools/md5.cpp %LFLAGS%
+
+IF %ERRORLEVEL% == 0 (
+    echo -- Running AoC Output --
+    bin\aoc.exe
+) else (
+    echo Compilation failed.
+)
